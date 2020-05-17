43be665a8b8ba3dd06c95e89bd0ebe1cb6162fd3
Glitch (thehexorcistadvancednode) <none>
2020-05-17 21:06:18 +0000

    💃🏄 Updated with Glitch

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ mongo.connect(process.env.DATABASE, (err, client) => {
             );
         });
       
+    
         passport.use(new LocalStrategy(
           function(username, password, done) {
             db.collection('users').findOne({ username: username }, function (err, user) {
@@ -77,9 +78,21 @@ mongo.connect(process.env.DATABASE, (err, client) => {
       
         app.route('/profile')
          .get(ensureAuthenticated, (req,res) => {
-            res.render(process.cwd() + '/views/pug/profile');
+            res.render(process.cwd() + '/views/pug/profile',{username: req.user.username});
          });
-
+      
+        app.route('/logout')
+          .get((req, res) => {
+            req.logout();
+            res.redirect('/');
+        });
+      
+        app.use((req, res, next) => {
+          res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
+      
         app.listen(process.env.PORT || 3000, () => {
           console.log("Listening on port " + process.env.PORT);
         });  
