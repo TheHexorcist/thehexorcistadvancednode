7fef6560cd7db398f127ecba7766bf78e4a8314e
Glitch (thehexorcistadvancednode) <none>
2020-05-28 23:45:59 +0000

    🐣🚡 Updated with Glitch

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const auth = require('./auth.js');
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 //const session     = require('express-session');
 //const passport    = require('passport');
 const mongo       = require('mongodb').MongoClient;
@@ -15,7 +14,6 @@ const mongo       = require('mongodb').MongoClient;
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
